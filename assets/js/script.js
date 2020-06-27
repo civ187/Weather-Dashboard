c0a070ca7ae2f0b50557f5806eb93b42a3ee97a1
Victor Cesar Lopez <civ187@gmail.com>
2020-06-26 23:13:58 -0500

    HTML 95% complete

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//accuweather api key 13bb915a529e6e3d8aaaafecaadcbe28
+//current weather api call
+//  api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+//  api.openweathermap.org/data/2.5/weather?q=san antonio&units=imperial&appid=13bb915a529e6e3d8aaaafecaadcbe28
+
+//Lattitude and longtitude caputured from this api and used to execute second api
+//lon: -98.49,
+//lat: 29.42
+
+//all in one api call  
+//  https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
+//  https://api.openweathermap.org/data/2.5/onecall?lat=29.42&lon=-98.49&units=imperial&exclude=minutely,hourly&appid=13bb915a529e6e3d8aaaafecaadcbe28
+
+// return the icons needed for the forcast
+// https://openweathermap.org/img/wn/{ICON CODE}@2x.png
